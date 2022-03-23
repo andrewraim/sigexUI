a9b2fc0b6ccc1267674bb0316cc3beff2b3cde54
jlivsey <james.a.livsey@gmail.com>
2022-03-23 13:22:22 -0400

    Write shell of sigexMLE function

--- R/SigexFit.R
+++ R/SigexFit.R
@@ -1,12 +1,44 @@
 # Constructor for class SigexFit object
 #' @export
-SigexFit = function(N) {
+SigexFit = function(optimOut, paramEst) {
 	new("SigexFit",
-		N = N,
-		gcds = list(),
-		ts_params = list(),
-		reg_param = numeric(0),
-		optim_output = list())
+		optim_output = optimOut,
+		param = paramEst)
 }
+
+
+
+SigexMLE <- function(SigexModel, data, SigexParam = NULL){
+
+
+	if(!is.null(SigexParam)) {
+		param <- to_sigex(SigexParm)
+	} else {
+		param <- sigex.default()
+	}
+
+
+	mdl <- to_sigex(SigexModel) # Needs to be written
+
+
+	st = Sys.time()
+	fit.mle = sigex.mlefit(data.ts = data,
+						   param = param,
+						   constraint = NULL,
+						   mdl = mdl,
+						   method = "bfgs",
+						   debug = TRUE)
+	et = Sys.time()
+	run_time <- as.numeric(et - st, units = "secs")
+
+	optimOut <- fit.mle[[1]]
+	paramEst <- asSigexParam(fit.mle[[2]], mdl) # Needs to be written
+
+	out <- SigexFit(optimOut, paramEst)
+
+	return(out)
+
+}
+
 
 
